bf414c8d79d175aa254b32682773bda1d9fe53bb
YauheniRusinchuk <ruevgal@gmail.com>
2019-04-25 06:32:15 +0300

    add cunsumers message

--- src/apps/room/comsumers.py
+++ src/apps/room/comsumers.py
@@ -1,15 +1,17 @@
 from channels.consumer import AsyncConsumer
 from channels.db import database_sync_to_async
 from src.models.room.models import Room
+from src.models.message.models import Message
 import json
 
 
 class CommentCunsumer(AsyncConsumer):
 
+
     async def websocket_connect(self, event):
         pk_room         = self.scope['url_route']['kwargs']['pk']
         room            = Room.objects.get(pk=pk_room)
-        self.post       = room
+        self.room       = room
         self.pk_room    = "id" + str(pk_room)
         await self.channel_layer.group_add(
             self.pk_room,
@@ -27,6 +29,32 @@ class CommentCunsumer(AsyncConsumer):
             load_data   = json.loads(text_comment)
             message     = load_data.get('message')
             user        = self.scope['user']
+            response = {
+                'message': message,
+                'username': user.username,
+            }
+        await self.create_comment(user, message)
+        await self.channel_layer.group_send(
+                self.pk_room,
+                {
+                    "type": "msg_comment",
+                    "text": json.dumps(response),
+                }
+            )
+
+
+    async def msg_comment(self, event):
+        #print('message', event)
+        await self.send({
+            "type": "websocket.send",
+            "text": event["text"]
+        })
+
+
+    @database_sync_to_async
+    def create_comment(self, user, msg):
+        Message.objects.create(author=user, text=msg, room=self.room)
+
 
 
     async def websocket_disconnect(self, event):
--- src/models/message/models.py
+++ src/models/message/models.py
@@ -9,8 +9,7 @@ class Message(models.Model):
     room            = models.ForeignKey(Room, on_delete=models.CASCADE)
     create_date     = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['-create_date']
+    
 
 
     def __str__(self):
